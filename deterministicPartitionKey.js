fb0a4d03de5ff45935dbe09aec2d9ecee3069cf0
Ingryd Moura <ingrydmoura1994@gmail.com>
2023-06-03 20:32:18 -0300

    refactor: change `deterministicPartitionKey` function

--- deterministicPartitionKey.js
+++ deterministicPartitionKey.js
@@ -1,29 +1,33 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
 
+function partitionKeyToString(event) {
+  let partitionKey = event.partitionKey; 
+
+  if (typeof event.partitionKey !== "string") {
+    partitionKey = JSON.stringify(event.partitionKey);
+  }
+
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    partitionKey = crypto.createHash("sha3-512").update(partitionKey).digest("hex");
+  }
+
+  return partitionKey;
+}
+
+exports.deterministicPartitionKey = (event) => {
   if (event) {
     if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+     return partitionKeyToString(event);
     }
+
+    const data = JSON.stringify(event);
+
+    return crypto.createHash("sha3-512").update(data).digest("hex");  
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return TRIVIAL_PARTITION_KEY;
 };
+
